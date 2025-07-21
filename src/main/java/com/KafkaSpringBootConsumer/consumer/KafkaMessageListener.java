7a843d054b9b616c68f6839ed8701666b5606231
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-07-21 23:05:16 +0530

    working on fixes

--- src/main/java/com/KafkaSpringBootConsumer/consumer/KafkaMessageListener.java
+++ src/main/java/com/KafkaSpringBootConsumer/consumer/KafkaMessageListener.java
@@ -1,6 +1,7 @@
 package com.KafkaSpringBootConsumer.consumer;
 
 
+import com.KafkaSpringBootConsumer.dto.Customer;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -10,25 +11,25 @@ import org.slf4j.Logger;
 public class KafkaMessageListener {
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
-    public void consumer1(String message) {
-        log.info("consumer1 receiving messages {}", message);
+    @KafkaListener(topics = "kafka-json-topics2", groupId = "kafka-consumer-grp-4json")
+    public void consumer(Customer customer) {
+        log.info("consumer receiving messages {}", customer);
     }
 
-    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
-    public void consumer2(String message) {
-        log.info("consumer2 receiving messages {}", message);
-    }
-
-    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
-    public void consumer3(String message) {
-        log.info("consumer3 receiving messages {}", message);
-    }
-
-    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
-    public void consumer4(String message) {
-        log.info("consumer4 receiving messages {}", message);
-    }
+//    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
+//    public void consumer2(String message) {
+//        log.info("consumer2 receiving messages {}", message);
+//    }
+//
+//    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
+//    public void consumer3(String message) {
+//        log.info("consumer3 receiving messages {}", message);
+//    }
+//
+//    @KafkaListener(topics = "kafka-con-prod-topics", groupId = "kafka-consumer-grp2")
+//    public void consumer4(String message) {
+//        log.info("consumer4 receiving messages {}", message);
+//    }
 
 
 }
---
+++ src/main/java/com/KafkaSpringBootConsumer/dto/Customer.java
@@ -0,0 +1,57 @@
+package com.KafkaSpringBootConsumer.dto;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", contactNo='" + contactNo + '\'' +
+                '}';
+    }
+
+    private String name;
+    private String email;
+    private String contactNo;
+
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
